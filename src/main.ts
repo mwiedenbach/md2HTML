f5125dc83171d1fc9a47f578bcaa3db5a3d09639
mwiedenbach <max.wiedenbach@gmail.com>
2026-08-08 13:59:41 +0200

    Fix: `file not found!` error. This error occurs, if the `settings.json` file does not exists. If the file exists the `tool` will transform the `file` to the `right` directory.

--- src/main.ts
+++ src/main.ts
@@ -63,9 +63,17 @@ function main(): void {
     const outputDirname: string = path.dirname(configContent?.outFile ?? outputPath);
     fs.mkdirSync(outputDirname, { recursive: true });
 
-    const outputFile: string = path.join(outputDirname, outputPath ?? outputPath);
+    console.log(`Debug: ${outputDirname}`);  
+    console.log(`Debug: ${outputPath}`);
+    console.log(`Debug: ${configContent?.outFile}`);  
 
-    console.log(outputFile);
+    // const outputFile: string = path.join(outputDirname, outputPath ?? outputPath);
+    const outputFile: string = configContent?.outFile ?? outputPath;
+
+    if (!fs.existsSync(inputFile)) {
+        console.error(`md2html ${inputFile}: Not found!`);
+        process.exit(1);
+    }
 
     const markdown = fs.readFileSync(inputFile);
     const html = convertMarkdownToHtml(markdown);
